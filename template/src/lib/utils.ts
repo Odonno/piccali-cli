879cdf4ff3cc9b53506bb5de7a5c780e745923ae
Odonno <bottiau.david@laposte.net>
2026-03-31 23:04:42 +0200

    ♻️ use slugify npm package

--- template/src/lib/utils.ts
+++ template/src/lib/utils.ts
@@ -1,16 +1,10 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import * as internalSlugify from "slugify";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
-/** Convert a display name to a URL-safe slug. */
-export function slugify(name: string): string {
-	return name
-		.toLowerCase()
-		.trim()
-		.replace(/[^\w\s-]/g, "")
-		.replace(/[\s_]+/g, "-")
-		.replace(/^-+|-+$/g, "");
-}
+export const slugify = (name: string) =>
+	internalSlugify.default(name, { lower: true });
