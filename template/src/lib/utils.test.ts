3d44da4a69396ac5829b377f3d0dab064eecd372
Odonno <bottiau.david@laposte.net>
2026-04-01 12:02:58 +0200

    🔧 configure vitest

---
+++ template/src/lib/utils.test.ts
@@ -0,0 +1,9 @@
+import { slugify } from "./utils";
+import { describe, expect, test } from "vitest";
+
+describe("slugify", () => {
+	test("Sélection d'un numéro VIN", () => {
+		const value = slugify("Sélection d'un numéro VIN");
+		expect(value).toBe("selection-d-un-numero-vin");
+	});
+});
---
+++ template/vitest.config.ts
@@ -0,0 +1,27 @@
+import path from "node:path";
+import { defineConfig } from "vitest/config";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import { playwright } from "@vitest/browser-playwright";
+
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		react(),
+		babel({ presets: [reactCompilerPreset()] }),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	test: {
+		browser: {
+			enabled: false,
+			provider: playwright(),
+			headless: true,
+			instances: [{ browser: "chromium" }],
+		},
+	},
+});
