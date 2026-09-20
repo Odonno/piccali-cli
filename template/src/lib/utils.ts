e7e35f159caba42e1fa7e5813674bb39692e28aa
Odonno <bottiau.david@laposte.net>
2026-09-20 21:58:54 +0200

    ♻️ upgrade to new cn utils package

--- template/src/lib/utils.ts
+++ template/src/lib/utils.ts
@@ -1,10 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
 import * as internalSlugify from "slugify";
 
-export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
-}
+export { cn } from "cn";
 
 export const slugify = (name: string) =>
 	internalSlugify.default(name, {
